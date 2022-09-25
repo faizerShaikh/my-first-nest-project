8c517bcdb40ebf5745437554cab84ede1ada8ebe
faizerShaikh <faizer.shaikh@fladdra.com>
2022-09-25 20:41:51 +0530

    fixed new changes

--- src/modules/posts/post.service.ts
+++ src/modules/posts/post.service.ts
@@ -13,43 +13,4 @@ export class PostService extends GenericService<Post, PostDto> {
       attributes: ['name', 'id', 'email', 'gender'],
     });
   }
-
-  // async create(post: PostDto): Promise<Post> {
-  //   return this.postModel.create<Post>(post);
-  // }
-
-  // async update(post: PostDto, id: number): Promise<Post> {
-  //   await this.postModel.update<Post>(post, { where: { id } });
-  //   const upadtedPost = await this.postModel.findOne<Post>({
-  //     where: { id },
-  //   });
-  //   return upadtedPost;
-  // }
-
-  // async findById(id: number): Promise<Post> {
-  //   return this.postModel.findOne<Post>({
-  //     where: { id },
-  //     include: {
-  //       model: User,
-  //       attributes: ['name', 'id', 'email', 'gender'],
-  //     },
-  //   });
-  // }
-
-  // async findAll(): Promise<Post[]> {
-  //   return this.postModel.findAll<Post>({
-  //     include: {
-  //       model: User,
-  //       attributes: ['name', 'id', 'email', 'gender'],
-  //     },
-  //   });
-  // }
-
-  // async delete(id?: number): number {
-  //   return this.postModel.destroy({
-  //     where: {
-  //       id,
-  //     },
-  //   });
-  // }
 }
--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -9,29 +9,4 @@ export class PostsController extends GenericController<Post, PostDto> {
   constructor(private readonly postService: PostService) {
     super(postService);
   }
-
-  // @Post('/')
-  // createPost(@Body() body: PostDto) {
-  //   return this.postService.create(body);
-  // }
-
-  // @Put('/:id')
-  // updatePost(@Body() body: PostDto, @Param('id') id: string) {
-  //   return this.postService.update(body, +id);
-  // }
-
-  // @Get('/:id')
-  // getOneById(@Param('id') id: number) {
-  //   return this.postService.findOne(+id);
-  // }
-
-  // @Get('/')
-  // getAll() {
-  //   return this.postService.findAll();
-  // }
-
-  // @Delete('/:id')
-  // delete(@Param('id') id: number) {
-  //   return this.postService.delete(+id);
-  // }
 }
